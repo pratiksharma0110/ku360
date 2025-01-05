bfde49c1f9291e5bcf21a7d4bc8f0a253c7981cd
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-05 11:55:45 +0545

    simple home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:ku360/services/login_or_register.dart';
+import 'package:ku360/services/shared_pref.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final SharedPrefHelper sharedPrefHelper = SharedPrefHelper();
+
+  Future<void> logout() async {
+    await sharedPrefHelper.removeValue("jwt_token");
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginOrRegister()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: logout, // Logout on press
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: logout,
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
