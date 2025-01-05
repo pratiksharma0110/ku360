d3eefac9b5a32357bd80b34ef83abb38f671851b
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-05 11:55:27 +0545

    persists user session

---
+++ lib/services/session_manager.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:ku360/pages/home.dart';
+import 'package:ku360/services/login_or_register.dart';
+
+import '../services/shared_pref.dart';
+
+class SessionHandlerPage extends StatefulWidget {
+  const SessionHandlerPage({Key? key}) : super(key: key);
+
+  @override
+  State<SessionHandlerPage> createState() => _SessionHandlerPageState();
+}
+
+class _SessionHandlerPageState extends State<SessionHandlerPage> {
+  final SharedPrefHelper sharedPrefHelper = SharedPrefHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkSession();
+  }
+
+  Future<void> _checkSession() async {
+    final token = await sharedPrefHelper.getValue("jwt_token");
+
+    if (token != null && token.isNotEmpty) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginOrRegister()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
