815f368bb59baf9511d678408b1421cd78662717
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-08 09:40:39 +0545

    main screen

---
+++ lib/pages/screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:ku360/components/appbar.dart'; // Your custom appbar
+import 'package:ku360/components/navbar.dart'; // Your custom navbar
+import 'package:ku360/pages/userPages/course_page.dart';
+import 'package:ku360/pages/userPages/home.dart';
+import 'package:ku360/pages/userPages/notice.dart';
+import 'package:ku360/pages/userPages/profile_screen.dart';
+
+class Screen extends StatefulWidget {
+  const Screen({super.key});
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  int selectedIndex = 0;
+
+  final List<String> appBarTitles = [
+    'Home',
+    'Course',
+    'Notices',
+    'Profile',
+  ];
+
+  final List<Widget> pages = [
+    HomePage(),
+    CoursesPage(),
+    NoticePage(),
+    ProfileScreen(),
+  ];
+
+  void _onTabChange(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: selectedIndex,
+        children: pages.map((page) {
+          return Scaffold(
+            appBar: MyAppBar(title: appBarTitles[selectedIndex]),
+            body: page,
+          );
+        }).toList(),
+      ),
+      bottomNavigationBar: MyNavBar(
+        selectedIndex: selectedIndex,
+        onTabChange: _onTabChange,
+      ),
+    );
+  }
+}
