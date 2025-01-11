9cf2aa7e52682493ea70409604d980abf9924252
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-11 15:37:12 +0545

    instructor name added

--- lib/model/routine.dart
+++ lib/model/routine.dart
@@ -2,14 +2,16 @@ class Routine {
   final String time;
   final String subCode;
   final String subName;
+  final String instructor;
 
-  Routine({required this.time, required this.subCode, required this.subName});
+  Routine({required this.time, required this.subCode, required this.subName,required this.instructor});
 
   factory Routine.fromJson(Map<String, dynamic> json) {
     return Routine(
       time: json['time'],
       subCode: json['sub_code'],
       subName: json['sub_name'],
+      instructor: json['instructor_name']
     );
   }
 }
